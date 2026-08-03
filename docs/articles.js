5b280a79361adfd8d891f78f7d1d74f032061d45
邓润庭 <1032939141@qq.com>
2026-08-03 11:43:39 +0800

    发布云端版本 2026-08-03 03:43

--- docs/articles.js
+++ docs/articles.js
@@ -82,6 +82,7 @@ export function initArticles({ apiFetch, apiBase, getToken, getDashboard, notify
           cdn: VDITOR_CDN,
           lang: "zh_CN",
           mode: "ir",
+          icon: "ant",
           theme: "classic",
           height: Math.max(580, window.innerHeight - 270),
           minHeight: 560,
